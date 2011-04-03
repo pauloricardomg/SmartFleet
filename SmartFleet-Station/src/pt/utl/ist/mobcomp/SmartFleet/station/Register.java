544528ea6b81fb8db519ebf1a1c0cdba03b9b5cd
Navaneeth <navaneeth.rameshan@gmail.com>
2011-04-03 20:11:09 +0100

    Manifest file fixed! Tried activity switching.

--- SmartFleet-Station/src/pt/utl/ist/mobcomp/SmartFleet/station/Register.java
+++ SmartFleet-Station/src/pt/utl/ist/mobcomp/SmartFleet/station/Register.java
@@ -1,13 +1,28 @@
 package pt.utl.ist.mobcomp.SmartFleet.station;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Register extends Activity {
-    /** Called when the activity is first created. */
+public class Register extends Activity implements OnClickListener{
+	
+	Button done;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+         done = (Button)this.findViewById(R.id.done);
+         done.setOnClickListener(this);      
     }
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = new Intent(this, Announce.class);
+		startActivity(intent);
+
+	}
 }
