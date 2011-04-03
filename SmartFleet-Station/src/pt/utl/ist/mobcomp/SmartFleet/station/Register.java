3ec5942fe7a10db01304a7ebcf8b0881f98db7d9
Navaneeth <navaneeth.rameshan@gmail.com>
2011-04-03 17:05:57 +0100

    Added the initial UI layout for registration at station

---
+++ SmartFleet-Station/src/pt/utl/ist/mobcomp/SmartFleet/station/Register.java
@@ -0,0 +1,13 @@
+package pt.utl.ist.mobcomp.SmartFleet.station;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Register extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
