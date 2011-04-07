d2aa424b34d4d1ac88d3918e80dc477497bc0c87
Paulo Ricardo Motta Gomes <pauloricardomg@gmail.com>
2011-04-07 16:26:51 +0100

    Added support to register stations on server and updated .gitignore and deleted useless files

--- SmartFleet-Server/server.py
+++ SmartFleet-Server/server.py
@@ -10,7 +10,8 @@ render = web.template.render('templates/', cache=False)
 # Registered server calls
 urls = (
     # http://serverIP:8080/RegisterParty?stationID=stationID;partyName=partyName;numPassengers=2;dest=abc METHOD: GET
-    '/RegisterParty', 'RegisterParty'
+    '/RegisterParty', 'RegisterParty',
+    '/RegisterStation', 'RegisterStation'
 )
 
 app = web.application(urls, globals())
@@ -18,7 +19,12 @@ app = web.application(urls, globals())
 class RegisterParty:
     def GET(self): # it is GET just for testing from the browser, change it later to POST
 	result = registerParty(web.input())
-        return render.RegisterPartyResponse(result)
+        return render.SimpleResponse(result)
+
+class RegisterStation:
+    def GET(self): # it is GET just for testing from the browser, change it later to POST
+	result = registerStation(web.input())
+        return render.SimpleResponse(result)
 
 web.webapi.internalerror = web.debugerror
 if __name__ == '__main__': app.run()
@@ -54,14 +60,45 @@ class Party:
 	def __eq__(self, other):
 		return self.name == other.name
 
+class Station:
+
+	def __init__(self, id, name, ip, port):
+		    self.id = id
+		    self.name = name
+		    self.ip = ip
+		    self.port = port
+
+	def __eq__(self, other):
+		return self.id == other.id
+
 # Server data structures
 
 vehicles = { 'vehicle1' : Vehicle('vehicle1',5),
 	     'vehicle2' : Vehicle('vehicle1',5) }
 
+activeStations = {}
+
 stationQueues = {}
 
 # Server interface
+
+def registerStation(input):
+	stationID = input.id
+	stationName = input.name
+	stationIP = input.ip
+	stationPort = input.port
+
+	# Verify if station is already registered
+	if(activeStations.get(stationID) != None):
+		print "Station " + thisParty.name + " already registered. Rejecting request."
+		return 0
+
+	# Add station to active stations
+	print "Registering station " + stationName + ". IP: " + stationIP + ". Port: " + stationPort 
+	activeStations[stationID] = Station(stationID, stationName, stationIP, stationPort)
+	print "Active stations " + str(activeStations)
+
+	return 1
 
 def registerParty(input):
 	# Retrieve station party queue
